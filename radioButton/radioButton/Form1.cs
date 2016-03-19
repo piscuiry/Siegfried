a4854c381c4f1e14f47e8cf1d95d9a2b2ec030ec
piscuiry <siegfriedro@outlook.com>
2016-03-19 01:06:21 -0600

    tooltip

---
+++ radioButton/radioButton/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace radioButton
+{
+    
+    public partial class Form1 : Form
+    {
+        private string categoria, dinero;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            categoria = "categoria B";
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            dinero = "100";
+        }
+
+        private void radioButton5_CheckedChanged(object sender, EventArgs e)
+        {
+            dinero = "$200";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("es de  "+ categoria +"  gana  "+ dinero);
+            label1.Text = "es de  " + categoria + "  gana  " + dinero;
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            categoria = "categoria A";
+        }
+    }
+}
